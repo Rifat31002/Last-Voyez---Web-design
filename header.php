16acf5d3726d341240de58d07af95eb588eb429e
Rifat <82029561+Rifat31002@users.noreply.github.com>
2022-11-11 19:04:07 +0000

    Add files via upload

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+    $document = JFactory::getDocument();
+?>
+    <header class="u-clearfix u-header u-header" id="sec-15eb">
+  <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+    <?php $logoInfo = getLogoInfo(array(
+            'src' => "/images/default-logo.png",
+            'href' => "https://nicepage.com",
+            'default_width' => '0'
+        ), true); ?><a href="<?php echo $logoInfo['href']; ?>" class="u-image u-logo u-image-1">
+      <img src="<?php echo $logoInfo['src']; ?>" class="u-logo-image u-logo-image-1">
+    </a>
+    <?php echo CoreStatements::position('hmenu', '', 1, 'hmenu'); ?>
+  </div>
+</header>
+    
---
+++ styles.php
@@ -0,0 +1,80 @@
+<style>
+.u-header {
+  background-image: none;
+}
+.u-header .u-sheet-1 {
+  min-height: 80px;
+}
+.u-header .u-image-1 {
+  margin: 22px auto 0 0;
+}
+.u-header .u-logo-image-1 {
+  max-width: 64px;
+  max-height: 64px;
+}
+.u-header .u-menu-1 {
+  margin: -32px 0 22px auto;
+}
+.u-header .u-nav-1 {
+  font-size: 1rem;
+  letter-spacing: 0px;
+}
+.u-block-20d7-19 {
+  box-shadow: 2px 2px 8px 0 rgba(128,128,128,1);
+}
+.u-header .u-nav-2 {
+  font-size: 1.25rem;
+}
+.u-block-20d7-17 {
+  box-shadow: 2px 2px 8px 0 rgba(128,128,128,1);
+}
+@media (max-width: 1199px) {
+  .u-header .u-image-1 {
+    width: 64px;
+    height: 32px;
+  }
+  .u-header .u-menu-1 {
+    width: auto;
+  }
+}
+@media (max-width: 767px) {
+  .u-header .u-image-1 {
+    width: auto;
+  }
+}
+</style>
+<style>
+.u-footer {
+  background-image: none;
+}
+.u-footer .u-sheet-1 {
+  min-height: 120px;
+}
+.u-footer .u-text-1 {
+  width: 417px;
+  margin: 49px auto;
+}
+@media (max-width: 1199px) {
+  .u-footer .u-sheet-1 {
+    min-height: 99px;
+  }
+}
+@media (max-width: 991px) {
+  .u-footer .u-sheet-1 {
+    min-height: 76px;
+  }
+}
+@media (max-width: 767px) {
+  .u-footer .u-sheet-1 {
+    min-height: 57px;
+  }
+}
+@media (max-width: 575px) {
+  .u-footer .u-sheet-1 {
+    min-height: 36px;
+  }
+  .u-footer .u-text-1 {
+    width: 340px;
+  }
+}
+</style>
